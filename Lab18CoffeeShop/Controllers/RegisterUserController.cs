779a7ef1f5e30447b4674b0c69cc6765fffa30af
Brandyn Tweedly <tweedly92@gmail.com>
2019-08-16 16:31:49 -0400

    First Commit

---
+++ Lab18CoffeeShop/Controllers/RegisterUserController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab18CoffeeShop.Controllers
+{
+    public class RegisterUserController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Register newRegister)
+        {
+            return RedirectToAction("Welcome", newRegister);
+        }
+
+        public IActionResult Welcome(Register register)
+        {
+            return View(register);
+        }
+    }
+}
